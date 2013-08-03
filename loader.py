34ae6afab2c6261fa7d0fd21231966fe65de74ab
Eric Lam <captainpicardlam@gmail.com>
2013-08-02 22:11:08 -0400

    Add print to loader to show loading

--- loader.py
+++ loader.py
@@ -7,10 +7,13 @@ def file_to_dict(path):
     word_file = open(path, 'r')
     dictionary = Dictionary()
 
+    counter = 0
     for line in word_file:
         if re.match('^[a-z]+$',line) is not None:
             dictionary.add_word(line.strip())
-
+        if counter % 25000 == 0:
+            print "Loading Dictionary..."
+        counter += 1
     dictionary.update_word_count()
     word_file.close()
     return dictionary
